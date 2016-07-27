80b7dbc88850af3f00d1c8cd5e2fe12aee737750
Ludovic Gouzille <ludovic.gouzille@aerian.com>
2016-07-27 08:48:54 +0100

    Adding page to display opportunity detail page

--- drupal/modules/custom/opportunity_search/src/Controller/OpportunityController.php
+++ drupal/modules/custom/opportunity_search/src/Controller/OpportunityController.php
@@ -1,23 +1,50 @@
 <?php
-
 namespace Drupal\opportunity_search\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\opportunity_search\Form\OpportunityForm;
+use Drupal\opportunity_search\Service\ElasticSearchService;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class OpportunityController extends ControllerBase
 {
-    public function content()
+    /**
+     * @var ElasticSearchService
+     */
+    protected $service;
+
+    public function __construct(ElasticSearchService $service)
+    {
+        $this->service = $service;
+    }
+
+    public static function create(ContainerInterface $container)
+    {
+        return new static(
+            $container->get('elastic_search_service')
+        );
+    }
+
+    public function form()
     {
         $form = \Drupal::formBuilder()->getForm(OpportunityForm::class);
 
         return [
-            '#theme'     => 'opportunity_search',
+            '#theme'     => 'opportunity_form',
             '#form'      => $form,
-            '#results'   => $form['results']['results'],
-            '#total'     => $form['results']['total'],
-            '#page'      => 5,
-            '#pageTotal' => ceil($form['results']['total'] / 10),
+            '#results'   => isset($form['results']) ? $form['results']['results'] : null,
+            '#total'     => isset($form['results']) ? $form['results']['total'] : null,
+            '#page'      => 1,
+            '#pageTotal' => isset($form['results']) ? ceil($form['results']['total'] / 10) : null,
+        ];
+    }
+
+    public function details($id)
+    {
+        return [
+            '#theme'       => 'opportunity_details',
+            '#opportunity' => $id,
+            '#attributes'  => [],
         ];
     }
 }
