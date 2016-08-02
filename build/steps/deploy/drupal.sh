ae4c16019c96b3805ac17137d99459d2435030c6
Ludovic Gouzille <ludovic.gouzille@aerian.com>
2016-08-02 16:26:04 +0100

    EEN-15: Adding Generation of config file

--- build/steps/deploy/drupal.sh
+++ build/steps/deploy/drupal.sh
@@ -12,5 +12,30 @@ cd $htdocs/drupal
 
 $htdocs/db/setup.sh
 
+cp $htdocs/drupal/sites/default/default.settings.php $htdocs/drupal/sites/default/settings.php
+
+# TODO Generate the hash
+echo "
+
+\$settings['hash_salt'] = 'HASH_SALT';
+
+\$settings['trusted_host_patterns'] = [
+    '^127.0.0.1$',
+    '^localhost$',
+    '^$hostname$'
+];
+
+\$databases['default']['default'] = array (
+  'database' => '$dbname',
+  'username' => '$dbuser',
+  'password' => '$dbpass',
+  'prefix' => '',
+  'host' => '$dbhost',
+  'port' => '',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+" >> $htdocs/drupal/sites/default/settings.php
+
 echo "Clearing drupal cache"
 $htdocs/bin/drush cr
