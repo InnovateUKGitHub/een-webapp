7de5d066b2b5b557eb7e9073d35c2828fa37f701
Ludovic Gouzille <ludovic.gouzille@aerian.com>
2016-08-15 16:54:48 +0100

    Adding custom file of drupal

---
+++ drupal/modules/custom/elastic_search/src/Service/ElasticSearchService.php
@@ -0,0 +1,140 @@
+<?php
+namespace Drupal\elastic_search\Service;
+
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Zend\Http\Client;
+use Zend\Http\Request;
+use Zend\Http\Response;
+
+class ElasticSearchService
+{
+    const SERVICE_ERROR_MSG = 'Cannot connect to elastic search.';
+    /** @var string */
+    private $baseUrl;
+    /** @var Client */
+    private $client;
+    /** @var int */
+    private $searchFrom = 0;
+    /** @var int */
+    private $searchSize = 10;
+
+    /**
+     * ElasticSearchService constructor.
+     */
+    public function __construct()
+    {
+        $config = \Drupal::config('elastic_search.settings');
+        $this->baseUrl = $config->get('server');
+        $this->client = new Client(null, ['timeout' => 30]);
+        $this->client->setHeaders([
+            'Content-type' => 'application/json',
+            'accept'       => 'application/json',
+        ]);
+        $this->client->setMethod(Request::METHOD_POST);
+    }
+
+    /**
+     * @param string $method
+     *
+     * @return $this
+     */
+    public function setMethod($method)
+    {
+        $this->client->setMethod($method);
+
+        return $this;
+    }
+
+    /**
+     * @param array $params
+     *
+     * @return $this
+     */
+    public function setQueryParams($params)
+    {
+        $this->client->setParameterGet($params);
+
+        return $this;
+    }
+
+    /**
+     * @param string $endPoint
+     *
+     * @return $this
+     */
+    public function setUrl($endPoint)
+    {
+        $this->client->setUri($this->baseUrl . $endPoint);
+
+        return $this;
+    }
+
+    /**
+     * @param array $query
+     *
+     * @return $this
+     */
+    public function setBody(array $query)
+    {
+        $this->client->setRawBody(json_encode($query));
+
+        return $this;
+    }
+
+    /**
+     * @param int $searchFrom
+     *
+     * @return $this
+     */
+    public function setSearchFrom($searchFrom)
+    {
+        $this->searchFrom = $searchFrom;
+
+        return $this;
+    }
+
+    /**
+     * @param int $searchSize
+     *
+     * @return $this
+     */
+    public function setSearchSize($searchSize)
+    {
+        $this->searchSize = $searchSize;
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function sendRequest()
+    {
+        $response = $this->client->send();
+        if (!$response->isSuccess()) {
+            // Throw correct error if applicable
+            return $this->manageErrorType($response);
+        }
+
+        return json_decode($response->getBody(), true);
+    }
+
+    /**
+     * @param Response $response
+     *
+     * @return array
+     */
+    private function manageErrorType(Response $response)
+    {
+        $error = json_decode($response->getBody(), true);
+
+        switch ($response->getStatusCode()) {
+            case Response::STATUS_CODE_404:
+                throw new NotFoundHttpException($error['detail']);
+            case Response::STATUS_CODE_422:
+                return ['error' => $error['validation_messages']];
+        }
+        // Return standard error message if no error type known
+        return ['error' => $error['detail']];
+    }
+}
---
+++ drupal/modules/custom/opportunities/src/Form/OpportunitiesForm.php
@@ -0,0 +1,111 @@
+<?php
+namespace Drupal\opportunities\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\elastic_search\Service\ElasticSearchService;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class OpportunitiesForm extends FormBase
+{
+    /**
+     * @var array
+     */
+    private $results;
+
+    /**
+     * @var ElasticSearchService
+     */
+    private $service;
+
+    /**
+     * OpportunitiesForm constructor.
+     *
+     * @param ElasticSearchService $service
+     */
+    public function __construct(ElasticSearchService $service)
+    {
+        $this->service = $service;
+    }
+
+    /**
+     * @param ContainerInterface $container
+     *
+     * @return OpportunitiesForm
+     */
+    public static function create(ContainerInterface $container)
+    {
+        return new self(
+            \Drupal::service('elastic_search.connection')
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFormId()
+    {
+        return 'opportunity_search_form';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(array $form, FormStateInterface $form_state)
+    {
+        $form = [
+            'search'  => [
+                '#type'     => 'textfield',
+                '#title'    => t('Search:'),
+                '#required' => true,
+            ],
+            'actions' => [
+                '#type'  => 'actions',
+                'submit' => [
+                    '#type'        => 'submit',
+                    '#value'       => $this->t('Search'),
+                    '#button_type' => 'primary',
+                ],
+            ],
+        ];
+        if ($form_state->getValue('search')) {
+            $form['results'] = $this->results;
+        }
+
+        return $form;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function validateForm(array &$form, FormStateInterface $form_state)
+    {
+        if (strlen($form_state->getValue('search')) < 1) {
+            $form_state->setErrorByName('search', t('Please enter at least 1 characters to perform a search.'));
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function submitForm(array &$form, FormStateInterface $form_state)
+    {
+        $values = $form_state->getValues();
+        $this->service->setUrl('opportunities')
+            ->setSearchParams([
+                'search' => $values['search'],
+                'sort'   => [
+                    ['date' => 'desc'],
+                ],
+                'source' => ['name', 'type', 'date', 'description', 'country', 'opportunity_type'],
+            ]);
+        $results = $this->service->sendRequest();
+        if (array_key_exists('error', $results)) {
+            drupal_set_message($results['error'], 'error');
+
+            return;
+        }
+        $this->results = $results;
+        $form_state->setRebuild();
+    }
+}
