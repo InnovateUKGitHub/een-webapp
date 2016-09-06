298ecf65e1ab8a1a90ba2b3d782ebdf11f180c64
Ludovic Gouzille <ludovic.gouzille@aerian.com>
2016-09-06 11:11:48 +0100

    Update gulp file changes to update js as well as css

--- build/steps/compile/gulp.sh
+++ build/steps/compile/gulp.sh
@@ -13,11 +13,13 @@ test -e compiled/drupal/themes/custom/een/scss || forceCompile=true
 
 # check for changes stripping out 'Common subdirectories' string
 csdTemplatesChanges=`diff --exclude="*.git*" -r drupal/themes/custom/een/scss compiled/drupal/themes/custom/een/scss | grep "Common subdirectories" -v || true`
+jsTemplatesChanges=`diff --exclude="*.git*" -r drupal/themes/custom/een/js compiled/drupal/themes/custom/een/js | grep "Common subdirectories" -v || true`
 
 # check if diff is empty (no changes)
-if [ ! -z "$csdTemplatesChanges" ] || [ ! -z "$forceCompile" ];then
-    echo "drupal/themes/custom/een/scss has changed:"
+if [ ! -z "$csdTemplatesChanges" ] || [ ! -z "$jsTemplatesChanges" ] || [ ! -z "$forceCompile" ];then
+    echo "drupal/themes/custom/een has changed:"
     echo $csdTemplatesChanges
+    echo $jsTemplatesChanges
     echo "running gulp"
 
     ./node_modules/gulp-cli/bin/gulp.js --verbose
