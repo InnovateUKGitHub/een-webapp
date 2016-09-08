e7c4259cd29c6866d76782b9d157ebd61e02d5cd
Hannah Williams <hannah.williams@aerian.com>
2016-09-08 17:44:08 +0100

    EEN-127 Scroll to postion

--- drupal/themes/custom/een/js/opportunity.js
+++ drupal/themes/custom/een/js/opportunity.js
@@ -10,4 +10,21 @@ jQuery(function () {
         hiddenField.val(phoneField.is(':visible') ? '0' : '1');
         phoneField.toggle();
     });
+    
+    
+    
+    $('.contents-list a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html, body').animate({
+                  scrollTop: target.offset().top
+                }, 700);
+              return false;
+            }
+        }
+    });
+    
+    
 });
