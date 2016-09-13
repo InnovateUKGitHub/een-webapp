691a7a558d89b37be168da6e7d4ab921f264fcba
Adam Smith <adam.smith@aerian.com>
2016-09-13 16:19:08 +0100

    added deployment build process

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,23 +6,33 @@ var gulp = require('gulp'),
     gulp = require('gulp'),
     sourcemaps = require('gulp-sourcemaps'),
     babel = require('gulp-babel'),
-    concat = require('gulp-concat'),
-    themeDir = 'drupal/themes/custom/een';
+    concat = require('gulp-concat');
+
+var themeDir = 'drupal/themes/custom/een';
+
+var entry = [
+  'node_modules/govuk_frontend_toolkit/javascripts/govuk/selection-buttons.js',
+  'node_modules/govuk_template_mustache/assets/javascripts/govuk-template.js',
+  'node_modules/angular/lib/angular.min.js',
+  (themeDir + '/js/**/*.js')
+];
 
 gulp.task('js', function () {
-    gulp.src([
-        'node_modules/govuk_frontend_toolkit/javascripts/govuk/selection-buttons.js',
-        'node_modules/govuk_template_mustache/assets/javascripts/govuk-template.js',
-        'node_modules/angular/lib/angular.min.js',
-        (themeDir + '/js/**/*.js')
-    ])
+    gulp.src(entry)
+    .pipe(babel({
+        ignore: ['node_modules'],
+        presets: ['es2015']
+    }))
+    .pipe(concat('bundle.js'))
+    .pipe(minify())
+    .pipe(gulp.dest(themeDir + '/dist'))
+});
+
+gulp.task('js-dev', function () {
+    gulp.src(entry)
     .pipe(sourcemaps.init())
     .pipe(babel({
-        ignore: [
-          'node_modules',
-          // 'node_modules/govuk_frontend_toolkit/javascripts/govuk/selection-buttons.js',
-          // 'node_modules/govuk_template_mustache/assets/javascripts/govuk-template.js'
-        ],
+        ignore: ['node_modules'],
         presets: ['es2015']
     }))
     .pipe(concat('bundle.js'))
@@ -54,7 +64,7 @@ gulp.task('css', function () {
 
 gulp.task('watch', function () {
     gulp.watch(themeDir + '/scss/**/*.scss', ['css']);
-    gulp.watch([themeDir + '/js/**/*.js'], ['js']);
+    gulp.watch([themeDir + '/js/**/*.js'], ['js-dev']);
 });
 
 gulp.task('default', ['css', 'js', 'img']);
