122549857bb2c87b6a1750420d28fca9097cb670
Adam Smith <adam.smith@aerian.com>
2016-09-16 10:13:39 +0100

    changing frontend code to handle new api endpoint format

--- drupal/themes/custom/een/js/angular-entry.js
+++ drupal/themes/custom/een/js/angular-entry.js
@@ -27,28 +27,47 @@
   een.controller('MainCtrl', function ($scope, oppsFactory) {
 
     $scope.submit = function (form) {
+      debugger;
       oppsFactory.search({
         page: 1,
         q: 'test'
       }).then(function (data) {
-        $scope.results = data;
+        $scope.heading = {
+          opportunity_type: data.opportunity_type,
+          page: data.page,
+          pageTotal: data.pageToal,
+          total: data.total
+        };
+
+        $scope.results = data.results;
+        debugger;
       }).fail(function(test) {
         $scope.results = [];
       });
+      return true;
     };
 
     $scope.heading = {
+      opportunity_type: null,
+      page: 1,
       loaded: true,
       total: 500
     };
 
     $scope.results = [
       {
-        title: 'Manufacturer of carbonated soft drinks and boza is looking for trade intermediary services.',
-        date: '4 days ago',
-        id: 'BOBG20150709001',
-        flag: 'bg',
-        country: 'Bulgaria'
+        'index': 'opportunity',
+        'type': 'opportunity',
+        'id': 'BOKR20150806001',
+        'score': 1,
+        'source': {
+          'summary': 'South Korean manufacturer of frozen yogurt powder is looking for distributors in Europe. Their products contain live probiotics which are good for the health and have passed strict control and certification tests. They are looking for a distributor in retail/food industry with well-established distribution channel. ',
+          'title': 'South Korean manufacturer of frozen yogurt powder is looking for distributors in Europe',
+          'type': 'BO',
+          'date': '2016-08-02T00:00:00',
+          'country_code': 'KR',
+          'country': 'South Korea'
+        }
       }
     ];
   });
