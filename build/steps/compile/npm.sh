923f27656a607807abae5bf31b8ec16225dce146
Adam Smith <adam.smith@aerian.com>
2016-09-27 15:23:00 +0100

    updated asset build process

--- build/steps/compile/npm.sh
+++ build/steps/compile/npm.sh
@@ -16,19 +16,6 @@ npmChanges=`diff package.json compiled/package.json || true`
 if [ ! -z "$npmChanges" ] || [ ! -z "$forceCompile" ];then
     echo "package.json has changed:"
     npm install --verbose
-
-    echo "copy gov images to theme folder"
-    test -e drupal/themes/custom/een/images || mkdir -p drupal/themes/custom/een/img
-    cp -r node_modules/govuk_frontend_toolkit/images/* drupal/themes/custom/een/img
-    cp -r node_modules/govuk_template_mustache/assets/images/* drupal/themes/custom/een/img
-    cp -r node_modules/govuk_template_mustache/assets/stylesheets/images/* drupal/themes/custom/een/img
-
-    echo "copy flags images to theme folder"
-    cp -r node_modules/flag-icon-css/flags drupal/themes/custom/een/
-
-    echo "copy fontawesome fonts into theme folder"
-    cp -r node_modules/font-awesome/fonts drupal/themes/custom/een/
-
 else
     echo "package.json has not changed, not running npm"
 fi
