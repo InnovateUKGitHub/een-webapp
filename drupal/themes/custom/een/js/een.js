64acd7f077c8ea0d328a9f7fcbcc94dc30723953
Hannah Williams <hannah.williams@aerian.com>
2016-10-03 13:06:52 +0100

    Reimplement js

---
+++ drupal/themes/custom/een/js/een.js
@@ -0,0 +1,14 @@
+jQuery(function () {
+    var $ = jQuery;
+
+    var buttons = $("label input[type='radio'], label input[type='checkbox']");
+    new GOVUK.SelectionButtons(buttons);
+    
+    
+    /* Basic toggle for form helper text */
+    $(document).on('click', '.field-prefix', function() {
+        var desc = $(this).next();
+        $(desc).toggle();
+    });
+});
+
