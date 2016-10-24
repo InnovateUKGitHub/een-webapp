52c97ecf9a22107d8a81f95fdce2d75018ba51d1
Ludovic Gouzille <l.gouzille@gmail.com>
2016-10-24 11:14:38 +0100

    Adding Banner "this is a prototype".

---
+++ drupal/modules/site_banner/src/SiteBannerController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\site_banner\SiteBannerController.
+ */
+
+namespace Drupal\site_banner;
+
+
+use Drupal\Core\Controller\ControllerBase;
+
+
+class SiteBannerController extends ControllerBase {
+}
